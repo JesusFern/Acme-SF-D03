7fbb43331aa3d9aa9fcbadc7c573dce959c2fe99
Jesus Fernandez <jesferrod1@alum.us.es>
2024-04-22 01:03:08 +0200

    user story feature implemented

--- src/main/java/acme/features/manager/project/ManagerProjectRepository.java
+++ src/main/java/acme/features/manager/project/ManagerProjectRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
 import acme.entities.projects.Project;
-import acme.entities.projects.UserStory;
+import acme.entities.projects.ProjectUserStory;
 import acme.roles.Manager;
 
 @Repository
@@ -26,8 +26,8 @@ public interface ManagerProjectRepository extends AbstractRepository {
 	@Query("select p from Project p where p.code = :code")
 	Project findOneProjectByCode(String code);
 
-	@Query("select pu.userStory from ProjectUserStory pu where pu.project.id = :projectId")
-	Collection<UserStory> findManyUserStoriesByProjectId(int projectId);
+	@Query("select pu from ProjectUserStory pu where pu.project.id = :projectId")
+	Collection<ProjectUserStory> findManyProjectUserStoriesByProjectId(int projectId);
 
 	@Query("select p from Project p where p.draftMode = false")
 	Collection<Project> findManyJobsByAvailability();
--- src/main/webapp/WEB-INF/views/fragments/menu.jsp
+++ src/main/webapp/WEB-INF/views/fragments/menu.jsp
@@ -47,6 +47,8 @@
 		<acme:menu-option code="master.menu.manager" access="hasRole('Manager')">	
 			<acme:menu-suboption code="master.menu.manager.my-projects" action="/manager/project/list-mine"/>
 			<acme:menu-suboption code="master.menu.manager.all-projects" action="/manager/project/list-all"/>
+			<acme:menu-separator/>
+			<acme:menu-suboption code="master.menu.manager.my-user-stories" action="/manager/user-story/list-mine"/>
 		</acme:menu-option>
 		
 		<acme:menu-option code="master.menu.developer" access="hasRole('Developer')">	
