beba90fee73e6ef27a8eb88044c4195addbd4e27
luiperfra1 <luiperfra1@alum.us.es>
2024-04-24 16:41:07 +0200

    Dashboard nearly done

--- src/main/java/acme/features/client/dashboard/ClientDashboardShowService.java
+++ src/main/java/acme/features/client/dashboard/ClientDashboardShowService.java
@@ -1,5 +1,5 @@
 
-package acme.features.client.dashboards;
+package acme.features.client.dashboard;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
